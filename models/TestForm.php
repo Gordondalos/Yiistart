ae65a096a7b8f387fb9bee7e04331d0600600a94
Evgenii Bondarenko <gordondalos@gmail.com>
2019-02-10 16:20:31 +0600

    валидация формы

--- models/TestForm.php
+++ models/TestForm.php
@@ -9,10 +9,44 @@ namespace app\models;
 
 use yii\base\Model;
 
-class TestForm extends Model
-{
+class TestForm extends Model {
     public $name;
     public $email;
     public $text;
+
+    // изменяем лейблы
+    public function attributeLabels() {
+        return [
+            'name' => 'Имя',
+            'email' => 'Ваше мыльце',
+            'text' => 'Ваше письмишко',
+        ];
+    }
+
+    public function rules() {
+        return [
+//            [['name', 'email'], 'required', 'message' => 'Поле обязательно' ],
+            [['name', 'email'], 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'min' => 5],
+            ['name', 'string',
+                'min' => 2,
+                'max' => 5,
+                'tooShort' => 'ППЦ короктое имя - неподходит',
+                'tooLong' => 'Херасе, a можно покороче?'],
+            ['text', 'string', 'length' => [2, 5]],
+
+            ['text', 'myRule'],
+            ['text', 'trim'],
+        ];
+    }
+
+    // эта валидация сработает только на сервере
+    public function myRule($attrs, $params) {
+        if(! in_array($this->$attrs, ['hello', 'world'])) {
+            $this->addError($attrs, 'wrong');
+        }
+    }
+
 
 }
--- views/post/index.php
+++ views/post/index.php
@@ -7,9 +7,9 @@ use yii\helpers\Html;
 
 <?php
 $form = ActiveForm::begin(['options' => ['id' => 'testForm']]);
-echo $form->field($model, 'name') -> label('Имя');
-echo $form->field($model, 'email') -> label('Мыло') -> input('email');
-echo $form->field($model, 'text') -> label('Ваш текст') -> textarea(['rows' => 5]);
+echo $form->field($model, 'name') ;
+echo $form->field($model, 'email') -> input('email');
+echo $form->field($model, 'text') -> textarea(['rows' => 5]);
 echo HTML::submitButton('Отправить', ['class' => 'btn btn-success']);
 
 ActiveForm::end();
