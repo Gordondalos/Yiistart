1c15e6c7767427140f325371f1adcf0e6135301d
Evgenii Bondarenko <gordondalos@gmail.com>
2019-02-10 15:03:02 +0600

    создание формы

---
+++ models/TestForm.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Gordondalos
+ * Date: 10.02.2019
+ * Time: 14:23
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+class TestForm extends Model
+{
+    public $name;
+    public $email;
+    public $text;
+
+}
--- views/post/index.php
+++ views/post/index.php
@@ -1,5 +1,16 @@
-Привет индекс
+<?php
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+?>
+
+<h1>Тест Экшн</h1>
 
 <?php
-//debug($names)
+$form = ActiveForm::begin(['options' => ['id' => 'testForm']]);
+echo $form->field($model, 'name') -> label('Имя');
+echo $form->field($model, 'email') -> label('Мыло') -> input('email');
+echo $form->field($model, 'text') -> label('Ваш текст') -> textarea(['rows' => 5]);
+echo HTML::submitButton('Отправить', ['class' => 'btn btn-success']);
+
+ActiveForm::end();
 ?>
